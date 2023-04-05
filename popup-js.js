e205c6a2488264ee6eb6df3b316e9741c99f88c0
Rishab Maheshwari <rishabmahesh@umass.edu>
2023-04-05 16:33:58 -0400

    add folder button added - functionality not working

--- popup-js.js
+++ popup-js.js
@@ -1,1 +1,7 @@
 console.log("Hello from popup-js.js")
+
+function addFolder() {
+  var button = document.createElement("button");
+  button.innerHTML = "New Button";
+  document.getElementById("folder-container").appendChild(button);
+}
