3848f9c2f7fca8aec88d4f604389fde4a73d6a40
Rajasi Desai <rajasi.desai18@gmail.com>
2023-04-05 16:58:53 -0400

    extension

--- popup-js.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello from popup-js.js")
-
-function addFolder() {
-  var button = document.createElement("button");
-  button.innerHTML = "New Button";
-  document.getElementById("folder-container").appendChild(button);
-}
