02a74fc08ca4e7b282d04dd1b88074e2d4e440b3
sspathak <suraj.s.pathak@gmail.com>
2023-04-05 22:23:50 -0400

    Containerzaiton

--- backend/app.py
+++ backend/app.py
@@ -48,7 +48,7 @@ def main():
     app = create_app()
     # app.run(host='127.0.0.1',port=6060, threaded=True)
     # uncomment code below to run flask with debugger
-    if os.environ.get('TESTING') != "1":
+    if os.environ.get('TESTING') == "1":
         app.run(host='0.0.0.0', port=8000, debug=True)  # Flask for debugging
     else:
         serve(app, port=8000)
