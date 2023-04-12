8aa42b1eb85d20b70a745612d0f16a7d0c4fcbd3
Rishab Maheshwari <rishabmahesh@umass.edu>
2023-04-11 20:05:43 -0400

    init

--- frontend/paperpal/src/App.js
+++ frontend/paperpal/src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Image } from '@chakra-ui/react'
 
 function App() {
+
+  const styles = {
+    extensionStyles: {
+      width: '721px',
+      height: '496px',
+      display: 'flex',
+      backgroundColor: '#F2F2F2',
+    },
+    topBarStyles: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      backgroundColor: '#13747A',
+      width: '721px',
+      height: '122px',
+    },
+    imageStyle: {
+      width: '202px',
+      height: '122px',
+    },
+    upperBox: {
+      width: '519px',
+      height: '122px',
+      backgroundColor: '#6CAFBE',
+    },
+  }
+
+
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div style={styles.extensionStyles}>
+      <div style={styles.topBarStyles}>
+        <Image src="../images/PaperPal-extension.png" alt="paperpal-logo" width={"202px"} height={"122px"} />
+        <div style={styles.upperBox}>
+          <h1>Hello world!</h1>
+        </div>
+      </div>
+
+
     </div>
   );
 }
