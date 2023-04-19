517909ca4c13fa7d72e9cfb83c96f3b1c7dcd691
Rajasi Desai <rajasi.desai18@gmail.com>
2023-04-19 15:46:57 -0400

    filter page

--- frontend/paperpal/src/components/FilterTab.js
+++ frontend/paperpal/src/components/FilterTab.js
@@ -118,13 +118,12 @@ export default function FilterTab(props) {
         <Button
             height={props.height}
             width={props.width}
-            bg='#45B19D'
+            bg='#296A5E'
             borderRadius='10px'
             fontSize='23px'
             color='#FFFFFF'
             border='1px'
             borderColor='#000000'
-            _hover={{ bg: '#378D7D' }}
             _active={{
             bg: '#307C6E',
             }}
