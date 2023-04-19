962cd175eb74eacfcaf3ee0aed10128a0c86a6af
Rishab Maheshwari <rishabmahesh@umass.edu>
2023-04-19 15:43:00 -0400

    dummy table added

--- frontend/paperpal/src/website.js
+++ frontend/paperpal/src/website.js
@@ -4,6 +4,30 @@ import { Image, Button } from '@chakra-ui/react'
 import NewFolderButton from './components/NewFolderButton'
 import DisplayPapersinFolder from './components/DisplayPapersinFolder'
 import ReactDOM from "react-dom";
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+
+const savedPaperColumns = [
+  { field: 'title', headerName: 'Title', width: 130 },
+  { field: 'authors', headerName: 'Authors', width: 130 },
+  { field: 'year', headerName: 'Year', width: 130 },
+  { field: 'citations', headerName: 'Citation no.', width: 130 },
+  { field: 'references', headerName: 'Reference no.', width: 130 },
+]
+
+const savedPaperRows = [
+  { id: 1, title: 'Paper 1', authors: 'Author 1', year: '2021', citations: '1', references: '1' },
+  { id: 2, title: 'Paper 2', authors: 'Author 2', year: '2021', citations: '1', references: '1' },
+  { id: 3, title: 'Paper 3', authors: 'Author 3', year: '2021', citations: '1', references: '1' },
+  { id: 4, title: 'Paper 4', authors: 'Author 4', year: '2021', citations: '1', references: '1' },
+  { id: 5, title: 'Paper 5', authors: 'Author 5', year: '2021', citations: '1', references: '1' },
+  { id: 6, title: 'Paper 6', authors: 'Author 6', year: '2021', citations: '1', references: '1' },
+  { id: 7, title: 'Paper 7', authors: 'Author 7', year: '2021', citations: '1', references: '1' },
+  { id: 8, title: 'Paper 8', authors: 'Author 8', year: '2021', citations: '1', references: '1' },
+  { id: 9, title: 'Paper 9', authors: 'Author 9', year: '2021', citations: '1', references: '1' },
+  { id: 10, title: 'Paper 10', authors: 'Author 10', year: '2021', citations: '1', references: '1' },
+  { id: 11, title: 'Paper 11', authors: 'Author 11', year: '2021', citations: '1', references: '1' },
+  { id: 12, title: 'Paper 12', authors: 'Author 12', year: '2021', citations: '1', references: '1' },
+]
 
 export default function Website(props) {
 
@@ -44,7 +68,7 @@ export default function Website(props) {
     },
     paperContainerBox: {
       height: `${window.innerHeight}px`,
-      width: `${window.innerWidth-325}px`,
+      width: `${window.innerWidth - 325}px`,
       marginLeft: '0px',
     }
   }
@@ -132,7 +156,16 @@ export default function Website(props) {
       </div>
 
       <div id="papers-container" style={styles.paperContainerBox}>
-        <h1>paper container</h1>
+        
+          <DataGrid
+            rows={savedPaperRows}
+            columns={savedPaperColumns}
+            pageSize={10}
+            slots={{
+              toolbar: GridToolbar,
+            }}
+          />
+        
       </div>
 
     </div>
