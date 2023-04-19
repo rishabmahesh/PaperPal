4766e2f150894a32c2a4ee09ab5bcc7bc0b9ffb1
sspathak <suraj.s.pathak@gmail.com>
2023-04-19 15:46:44 -0400

    added recommendations endpoint

--- backend/src/data/query_helper.py
+++ backend/src/data/query_helper.py
@@ -10,7 +10,7 @@ author_paper = pd.read_csv("./database/BiblioVIS/CSV_Author_Paper.csv")
 author_paper_affiliation = pd.read_csv("./database/BiblioVIS/CSV_Paper_Author_Affiliation.csv")
 csm_df = get_cosine_similarity_matrix(as_df=True, intermediate_folder_path="./database/mapped_intermediate_folder")
 # convert csm_df to matrix
-csm_df = csm_df.pivot(index='Paper_ID_1', columns='Paper_ID_2', values='Cosine_Similarity').fillna(0)
+csm_df = csm_df.pivot(index='Paper_ID_1', columns='Paper_ID_2', values='Cosine_Similarity').fillna(1)
 
 def get_info_from_id(paper_id):
     authors = paper[paper["Paper_ID"] == int(paper_id)].merge(author_paper, on="Paper_ID").merge(author, on="Author_ID")['Name'].values
@@ -39,7 +39,7 @@ def get_recommendations(paper_id_arr, n):
     top_n_for_each_paper = {pid: [] for pid in paper_id_arr}
     all_paper_ids = get_all_paper_ids()
     for pid in paper_id_arr:
-        top_n_for_each_paper[pid] = csm_df.loc[pid].sort_values(ascending=False)[1:n+1].index.values
+        top_n_for_each_paper[pid] = json.loads(csm_df.loc[pid].sort_values(ascending=False)[1:n+1].to_json())
     return top_n_for_each_paper
 
 
