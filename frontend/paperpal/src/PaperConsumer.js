af68ea04891cf18407574b13f02853c86ad5eefa
sspathak <suraj.s.pathak@gmail.com>
2023-05-04 11:14:31 -0400

    bugfix 2 cors or 502 error

--- frontend/paperpal/src/PaperConsumer.js
+++ frontend/paperpal/src/PaperConsumer.js
@@ -3,23 +3,8 @@ import axios from 'axios';
 const API_MAIN = 'https://paperpal-qfjdfiwtma-uc.a.run.app';
 
 export default class PaperConsumer {
-
   static async getPaperInfo(paperID) {
-    const response = await axios.post(
-      `${API_MAIN}/info`, 
-      {
-        paperID
-      },
-      {
-        headers: {
-          'mode': 'no-cors'
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-    console.log("eee ", response.data)
+    const response = await axios.post(`${API_MAIN}/info`, paperID);
     return response.data;
   }
 
